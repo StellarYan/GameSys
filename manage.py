bd7ead8ce7045f3fde1cf4989870dc50354c8509
Jackieyang <codeofjackie@users.noreply.github.com>
2017-11-24 12:56:11 +0800

    new

--- manage.py
+++ manage.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GameSys.settings")
     try:
